32c7447018d4149bb0d6a50102e00a6849d9c2bc
sheepkill15 <simonarons15@gmail.com>
2022-06-12 10:10:15 +0300

    allow multiple instances

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod overlay;
 
 use std::env;
 
+use gdk::gio::ApplicationFlags;
 use gtk::prelude::*;
 use overlay::Overlay;
 
@@ -48,30 +49,49 @@ fn main() {
         }
     }
 
-    let application = gtk::Application::new(Some("com.github.sheepkill15.osd"), Default::default());
+    let application = gtk::Application::new(
+        Some("com.github.sheepkill15.osd"),
+        ApplicationFlags::NON_UNIQUE
+            | ApplicationFlags::REPLACE
+            | ApplicationFlags::ALLOW_REPLACEMENT,
+    );
     application.connect_activate(move |app| {
         build_ui(app, &value, &text, &icon);
     });
     application.run_with_args(&[""]);
 }
 
-fn build_ui(application: &gtk::Application, val: &Option<String>, text: &Option<String>, icon: &Option<String>) {
-    let val_conv = if let Some(value) = val {Some(value.to_string())} else {None}; 
-    let text_conv = if let Some(value) = text {Some(value.to_string())} else {None}; 
-    let icon_conv = if let Some(value) = icon {Some(value.to_string())} else {None}; 
-    let _overlay = Overlay::new(
-        application,
-        val_conv,
-        text_conv,
-        icon_conv,
-    );
+fn build_ui(
+    application: &gtk::Application,
+    val: &Option<String>,
+    text: &Option<String>,
+    icon: &Option<String>,
+) {
+    let val_conv = if let Some(value) = val {
+        Some(value.to_string())
+    } else {
+        None
+    };
+    let text_conv = if let Some(value) = text {
+        Some(value.to_string())
+    } else {
+        None
+    };
+    let icon_conv = if let Some(value) = icon {
+        Some(value.to_string())
+    } else {
+        None
+    };
+    let _overlay = Overlay::new(application, val_conv, text_conv, icon_conv);
 }
 
 fn print_help() {
-    print!(r#"Correct usage:
+    print!(
+        r#"Correct usage:
     -v [value]      Specify percentage value
     -t [value]      Specify text to display
     -i [value]      Specify icon file or name
-    "#);
+    "#
+    );
     println!();
 }
